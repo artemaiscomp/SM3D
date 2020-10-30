e6422b3bf1e76eca1d610f0112a4c6007c8a02d9
Lucas <lucasqueiroz@alu.ufc.br>
2020-10-30 02:09:41 -0300

    Lucas Queiroz collaborating + Monolith with text (static)

--- Assets/Script/TesteListaMaterial.cs
+++ Assets/Script/TesteListaMaterial.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TesteListaMaterial : MonoBehaviour
 {
@@ -12,41 +13,61 @@ public class TesteListaMaterial : MonoBehaviour
     public GameObject obj_janela;
     public GameObject[] obj_elevador = new GameObject[2];
     public GameObject obj_pedregulho;
-    public Material[] TransfText_parede = new Material [8];
-    public Material[] TransfText_Muro = new Material [8];
-    public Material[] TransfText_chao = new Material [8];
-    public Material[] TransfText_madeira = new Material [8];
-    public Material[] TransfText_janela = new Material [8];
-    public Material[] TransfText_elevador = new Material [8];
-    public Material[] TransfText_pedras = new Material [8];
-        void Start()
+    public Material[] TransfText_parede = new Material[8];
+    public Material[] TransfText_Muro = new Material[8];
+    public Material[] TransfText_chao = new Material[8];
+    public Material[] TransfText_madeira = new Material[8];
+    public Material[] TransfText_janela = new Material[8];
+    public Material[] TransfText_elevador = new Material[8];
+    public Material[] TransfText_pedras = new Material[8];
+
+    //Change text Variable
+    public Text txtMonolith;
+
+    void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    
 
-    void OnTriggerEnter(Collider other){
-        
-        for(int i=0; i<gmBloco.Length; i++){
-        gmBloco[i].GetComponent<Renderer>().material = TransfText_parede[IndicePorta];
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        for (int i = 0; i < gmBloco.Length; i++)
+        {
+            gmBloco[i].GetComponent<Renderer>().material = TransfText_parede[IndicePorta];
         }
-        for(int i=0; i<obj_elevador.Length; i++){
-        obj_elevador[i].GetComponent<Renderer>().material = TransfText_elevador[IndicePorta];
+        for (int i = 0; i < obj_elevador.Length; i++)
+        {
+            obj_elevador[i].GetComponent<Renderer>().material = TransfText_elevador[IndicePorta];
         }
-        for(int i=0; i<objParedes.Length; i++){
-        objParedes[i].GetComponent<Renderer>().material = TransfText_Muro[IndicePorta];
+        for (int i = 0; i < objParedes.Length; i++)
+        {
+            objParedes[i].GetComponent<Renderer>().material = TransfText_Muro[IndicePorta];
         }
 
-        obj_chao.GetComponent<Renderer>().material = TransfText_chao [IndicePorta];
-        obj_madeira.GetComponent<Renderer>().material = TransfText_madeira [IndicePorta];
-        obj_pedregulho.GetComponent<Renderer>().material = TransfText_pedras [IndicePorta];
-        obj_janela.GetComponent<Renderer>().material = TransfText_janela [IndicePorta];
+        obj_chao.GetComponent<Renderer>().material = TransfText_chao[IndicePorta];
+        obj_madeira.GetComponent<Renderer>().material = TransfText_madeira[IndicePorta];
+        obj_pedregulho.GetComponent<Renderer>().material = TransfText_pedras[IndicePorta];
+        obj_janela.GetComponent<Renderer>().material = TransfText_janela[IndicePorta];
+
+        //Change Text
+
+        switch (other.name)
+        {
+            case "FPSController":
+                txtMonolith.text = "Entrou na porta da Noite estrelada, parabéns, doido";
+                break;
+            default:
+                txtMonolith.text = "Outra porta qualquer";
+                break;
+        }
     }
-        
+
 }
